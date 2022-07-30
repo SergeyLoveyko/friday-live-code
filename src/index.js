51ab52d023bd1d5d159274ea78434b7d5fcd2811
Sergey <loveykosergey@ukr.net>
2022-07-31 02:18:37 +0300

    feat: add sorting cards by tag in section strategies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-console.log( 'Hello Rolling Scopes!' );
+window.onload = function() {
+  console.log( 'Hello Rolling Scopes!' );
+
+  //  Tags
+  addTagsClickHandler();
+}
+
+const addTagsClickHandler = () => {
+
+  document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+    if (e.target.classList.contains('tag')) {
+      let clickedTag = e.target;
+      removeSelectedTags();
+      selectClickedTag(clickedTag);
+
+      if (clickedTag.innerText === 'All') {
+        showAllStrategies();
+      } else {
+        filterStrategiesBySelectedTag(clickedTag.innerText);
+      }
+    }
+  });
+};
+
+const removeSelectedTags = () => {
+  let tags = document.querySelectorAll('.strategies__tags .tag');
+  tags.forEach(tag => {
+    tag.classList.remove('tag_selected');
+    tag.classList.add('tag_bordered');
+  });
+};
+
+const selectClickedTag = (clickedTag) => {
+  clickedTag.classList.remove('tag_bordered');
+  clickedTag.classList.add('tag_selected');
+};
+
+const showAllStrategies = () => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach(strategy => {
+    strategy.classList.remove('strategy_hidden');
+  })
+}
+
+const filterStrategiesBySelectedTag = (selectedTag) => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach(strategy => {
+    strategy.classList.add('strategy_hidden');
+    console.log( strategy );
+    strategy.querySelectorAll('.tag').forEach(tag => {
+      if (tag.innerText === selectedTag) {
+        strategy.classList.remove('strategy_hidden');
+      }
+    });
+  });
+};
